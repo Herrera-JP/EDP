937a1ab0d1e492e0288e931b3bf40c3a9e2fef70
Herrera-JP <j0hnp4tr1ck23@gmail.com>
2026-09-05 00:03:02 +0800

    Add files via upload

---
+++ QueuingForm/QueuingForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace QueuingForm
+{
+    partial class QueuingForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCashier = new Button();
+            lblQueue = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnCashier
+            // 
+            btnCashier.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCashier.Location = new Point(23, 32);
+            btnCashier.Name = "btnCashier";
+            btnCashier.Size = new Size(138, 120);
+            btnCashier.TabIndex = 0;
+            btnCashier.Text = "Casier";
+            btnCashier.UseVisualStyleBackColor = true;
+            btnCashier.Click += btnCashier_Click;
+            // 
+            // lblQueue
+            // 
+            lblQueue.AutoSize = true;
+            lblQueue.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblQueue.Location = new Point(205, 83);
+            lblQueue.Name = "lblQueue";
+            lblQueue.Size = new Size(102, 31);
+            lblQueue.TabIndex = 1;
+            lblQueue.Text = "P-00000";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(178, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 28);
+            label1.TabIndex = 2;
+            label1.Text = "Position in Queue";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(23, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 17);
+            label2.TabIndex = 3;
+            label2.Text = "*Click to get a number";
+            // 
+            // QueuingForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(431, 261);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lblQueue);
+            Controls.Add(btnCashier);
+            Name = "QueuingForm";
+            Text = "QueuingForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCashier;
+        private Label lblQueue;
+        private Label label1;
+        private Label label2;
+    }
+}
